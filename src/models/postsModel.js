59ca867f3c9b11a704bc2823f00407eaaca07916
Manoel Bezerra <manoelbezezeraapps@gmail.com>
2024-11-23 19:05:37 -0300

    deploy

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,4 +1,6 @@
 // Importa a função para conectar ao banco de dados MongoDB
+import "dotenv/config";
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbconfig.js";
 // Conecta ao banco de dados MongoDB usando a string de conexão do ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
@@ -12,8 +14,16 @@ export  async function getTosdosPosts() {
     // Busca todos os documentos da coleção e retorna como um array
     return colecao.find().toArray();
 }
+
 export async function criarPost(novoPost) {
     const db = conexao.db("aulanodejs");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("aulanodejs");
+    const colecao = db.collection("posts")
+    const objId = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(objId)}, {$set:novoPost}) 
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express"
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsControllers.js";
+import cors from "cors"
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsControllers.js";
+
+const corsOptions ={
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb){
@@ -16,10 +22,12 @@ const upload = multer({dest:"./uploads", storage});
 const routes = (app) => {
     // Habilita o parser JSON para lidar com requisições JSON
     app.use(express.json());
+    app.use(cors(corsOptions))
     // Define uma rota GET para "/posts"
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost);
     app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost)
 }
 
 export default routes;
